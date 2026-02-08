94918aeddc14bea0665e97e0a16e2c730db807e2
Phanith NY <phanith.ny@wingbank.com.kh>
2026-02-08 23:05:22 +0700

    updated config

--- Sources/EasyAnchor/Config.swift
+++ Sources/EasyAnchor/Config.swift
@@ -11,18 +11,21 @@ public protocol Config { }
 extension NSObject: Config { }
 
 extension Config where Self: NSObject {
-  /// Makes it available to set properties with closures just after initializing.
-  ///
-  ///     let label = UILabel().decorate {
-  ///       $0.textAlignment = .center
-  ///       $0.textColor = .black
-  ///       $0.text = "Hi There!"
-  ///     }
+  @discardableResult
   public func decorate(_ closure: (Self) -> Void) -> Self {
     closure(self)
     return self
   }
   
+  /// Makes it available to set properties with closures just after initializing.
+  ///
+  ///     let label = UILabel().config {
+  ///       $0.textAlignment = .center
+  ///       $0.textColor = .black
+  ///       $0.text = "Hi There!"
+  ///     }
+  ///
+  @discardableResult
   public func config(_ closure: (Self) -> Void) -> Self {
     closure(self)
     return self
